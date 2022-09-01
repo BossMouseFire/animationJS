e84da704d99d8cd6d6757e68c6c2a10b87f8170c
BossMouseFire <abanin-02@mail.ru>
2022-09-01 13:23:36 +0400

    change model

--- src/models/ball.ts
+++ src/models/ball.ts
@@ -1,41 +1,28 @@
-import Model from "./model";
+import Model, {IModel} from "./model";
 
 
-export default class Ball implements Model {
-    public x: number;
-    public y: number;
-    public dy: number;
-    public dx: number;
+export default class Ball extends Model implements IModel {
     public radius: number;
-    public color: string;
-    private angle: number;
-    private speed: number;
-    private readonly mass: number
 
     constructor(x: number, y: number,
                 radius: number, color: string, angle: number, speed: number, mass: number) {
-        this.x = x;
-        this.y = y;
+        super(x, y, color, angle, speed, mass)
         this.radius = radius;
-        this.color = color;
-        this.angle = angle
-        this.speed = speed;
-        this.mass = mass
         this.changeUnits();
     }
 
     public update(width: number, height: number, context: CanvasRenderingContext2D) {
-        if (Model.wrapDevicePixelRation(this.y + this.radius) > height) {
+        if (this.wrapDevicePixelRation(this.y + this.radius) > height) {
             this.speed = this.speed * Model.friction
         }
 
-        if (Model.wrapDevicePixelRation(this.x + this.radius) > width
-            || Model.wrapDevicePixelRation(this.x - this.radius) < 0) {
+        if (this.wrapDevicePixelRation(this.x + this.radius) > width
+            || this.wrapDevicePixelRation(this.x - this.radius) < 0) {
             this.angle = 180 - this.angle;
             this.changeUnits();
         } else if (
-            Model.wrapDevicePixelRation(this.y + this.radius) > height
-            || Model.wrapDevicePixelRation(this.y - this.radius) < 0) {
+            this.wrapDevicePixelRation(this.y + this.radius) > height
+            || this.wrapDevicePixelRation(this.y - this.radius) < 0) {
             this.angle = 360 - this.angle;
             this.changeUnits();
         }
@@ -53,9 +40,9 @@ export default class Ball implements Model {
     public draw(context: CanvasRenderingContext2D) {
         context.beginPath()
         context.arc(
-            Model.wrapDevicePixelRation(this.x),
-            Model.wrapDevicePixelRation(this.y),
-            Model.wrapDevicePixelRation(this.radius),
+            this.wrapDevicePixelRation(this.x),
+            this.wrapDevicePixelRation(this.y),
+            this.wrapDevicePixelRation(this.radius),
             0,
             2 * Math.PI
         )
--- src/models/model.ts
+++ src/models/model.ts
@@ -1,14 +1,34 @@
 
 
-export default abstract class Model {
-    public static readonly friction: number = 0.95;
+export default class Model {
+    public x: number;
+    public y: number;
+    protected dy: number;
+    protected dx: number;
+    public color: string;
+    protected angle: number;
+    protected speed: number;
+    public readonly mass: number
 
-    public static wrapDevicePixelRation(number: number) {
+    protected static readonly friction: number = 0.95;
+
+    constructor(x: number, y: number, color: string, angle: number, speed: number, mass: number) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.angle = angle;
+        this.speed = speed;
+        this.mass = mass;
+    }
+
+    protected wrapDevicePixelRation(number: number) {
         return number * devicePixelRatio
     }
 
-    public abstract draw(context: CanvasRenderingContext2D): void
-
-    public abstract update(width: number, height: number, context: CanvasRenderingContext2D): void
-
 }
+
+export interface IModel {
+    draw(context: CanvasRenderingContext2D): void;
+    update(width: number, height: number, context: CanvasRenderingContext2D): void;
+}
+
